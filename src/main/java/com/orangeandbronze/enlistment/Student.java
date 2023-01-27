28d2157d496d23123a701049978bcfb7a2b897fe
Ultix1 <92718418+Ultix1@users.noreply.github.com>
2023-01-27 15:49:01 +0800

    Truly the work of sergei

--- src/main/java/com/orangeandbronze/enlistment/Student.java
+++ src/main/java/com/orangeandbronze/enlistment/Student.java
@@ -1,5 +1,12 @@
 package com.orangeandbronze.enlistment;
 
 public class Student {
+    private final int studentNumber;
 
+    Student(int studentNumber){
+        if(studentNumber < 0){
+            throw new IllegalArgumentException("studentNumber should be non-negative, was: " + studentNumber);
+        }
+        this.studentNumber=studentNumber;
+    }
 }
